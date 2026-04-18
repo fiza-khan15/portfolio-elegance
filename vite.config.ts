875575e2a5443e29d5e26256290b3772d34a3081
fiza-khan15 <fizanazimkhan@gmail.com>
2026-04-18 13:04:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  // This ensures Vite builds a standard static 'dist' folder
+  server: {
+    // This tells TanStack/Vite NOT to build a server-side version
+    ssr: false, 
+  },
   build: {
+    // This ensures all files go into /dist/ and not /dist/server/
     outDir: 'dist',
+    ssr: false,
   }
 });
